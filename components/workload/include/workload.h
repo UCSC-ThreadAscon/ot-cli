80a4242da36f8ac27e3404783611be5e8e1f63a9
Simeon Tran <siatran@ucsc.edu>
2024-04-06 16:26:57 -0700

    Got handler to work with no build errors.

--- components/workload/include/workload.h
+++ components/workload/include/workload.h
@@ -14,6 +14,40 @@
 
 #include "x509_cert_key.h"
 
+/**
+ * The statement to print when a CoAP request is received is as follows:
+ * 
+ *    "Received [uint32_t] bytes from [IPv6 address].".
+ *
+ * The substring "Received " is 9 bytes.
+ * The substring " bytes from " is 12 bytes.
+ * The substring "." is 1 byte.
+ *
+ * The 32 bit integer will be represented as a string.
+ * 2^32 - 1 = 4294967295 has 10 digits, and thus, the
+ * string representation of the uint32_t will make up 10 bytes.
+ *
+ * Furthermore, an IPv6 string representation is made up of 16 bytes.
+ *
+ * Thus, the total string size is  9 + 12 + 1 + 10 + 40 = 72 bytes.
+*/
+#define PRINT_STATEMENT_SIZE 72
+
+/**
+ * Empties all memory for `size` bytes starting at memory address `pointer`.
+ *
+ * @param[in] pointer: the pointer of the stack memory
+ * @param[in] size:    the size of the memory that `pointer` points to
+ *
+ * I got the idea to use `memset()` to clear stack memory from
+ * the Google Search AI:
+ * https://docs.google.com/document/d/1o-NaEOA-vzWPCv7VX1dONUfwos2epveDk4H_Y2Y5g1Y/edit?usp=sharing
+*/
+static inline void EmptyMemory(void* pointer, size_t size) {
+  memset(pointer, 0, size);
+  return;
+}
+
 #define COAP_SOCK_PORT 12345
 #define COAP_DEST_PORT 54321
 
--- components/workload/server.c
+++ components/workload/server.c
@@ -1,5 +1,26 @@
-#include <stdio.h>
 #include "workload.h"
+
+#include <stdio.h>
+#include <string.h>
+#include "stdint.h"
+#include "inttypes.h"
+
+static inline uint16_t getPayloadLength(const otMessage *aMessage) {
+  return otMessageGetLength(aMessage) - otMessageGetOffset(aMessage);
+}
+
+void periodicRequestHandler(void *aContext, otMessage *aMessage, const otMessageInfo *aMessageInfo) {
+  uint32_t length = getPayloadLength(aMessage);
+
+  otIp6Address sender = aMessageInfo->mSockAddr;
+  char senderAddressString[OT_IP6_ADDRESS_STRING_SIZE];
+  otIp6AddressToString(&sender, senderAddressString, OT_IP6_ADDRESS_STRING_SIZE);
+
+  char output[PRINT_STATEMENT_SIZE];
+  sprintf(output, "Received %" PRIu32 " bytes from %s", length, (char *) &senderAddressString);
+  otLogNotePlat(output);
+  return;
+}
 
 otError periodicResource(otCoapResource *periodic) {
   periodic->mNext = NULL;
@@ -8,7 +29,7 @@ otError periodicResource(otCoapResource *periodic) {
   periodic->mUriPath = "periodic";
 
   // TO-DO: Create Handler
-  periodic->mHandler = NULL;
+  periodic->mHandler = periodicRequestHandler;
   return OT_ERROR_NONE;
 }
 
