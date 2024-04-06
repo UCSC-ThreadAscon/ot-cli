8e8f9eee276442f767b569b6503af0535769bab7
Simeon Tran <siatran@ucsc.edu>
2024-04-06 15:51:55 -0700

    Initialize x509 key

--- main/esp_ot_cli.c
+++ main/esp_ot_cli.c
@@ -12,6 +12,7 @@
  * CONDITIONS OF ANY KIND, either express or implied.
 */
 #include "txpower.h"
+#include "workload.h"
 
 #include <stdio.h>
 #include <unistd.h>
@@ -130,4 +131,7 @@ void app_main(void)
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_vfs_eventfd_register(&eventfd_config));
     xTaskCreate(ot_task_worker, "ot_cli_main", 10240, xTaskGetCurrentTaskHandle(), 5, NULL);
+
+    /** ---- Set up the CoAP Server ---- */
+    x509Init();
 }
