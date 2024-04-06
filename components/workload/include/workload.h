eb68ed47d16f0ce14effa7198c545211160b595e
Simeon Tran <siatran@ucsc.edu>
2024-04-06 15:12:42 -0700

    Builds so far

--- components/workload/include/workload.h
+++ components/workload/include/workload.h
@@ -1,1 +1,25 @@
-void func(void);
+#include <openthread/logging.h>
+
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_openthread.h"
+#include "esp_openthread_types.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "openthread/cli.h"
+#include "openthread/instance.h"
+#include "openthread/logging.h"
+#include "openthread/tasklet.h"
+#include "openthread/coap_secure.h"
+
+#define COAP_SOCK_PORT 12345
+#define COAP_DEST_PORT 54321
+
+#define OT_DISCONNECTED(role) (role == OT_DEVICE_ROLE_DISABLED) || (role == OT_DEVICE_ROLE_DETACHED)
+
+#define MS_TO_TICKS(ms) ms / portTICK_PERIOD_MS
+#define MS_TO_MICRO(ms) ms * 1000
+
+#define CONNECTION_WAIT_TIME_MS MS_TO_TICKS(100)
+
+void checkConnection(otInstance *aInstance);
