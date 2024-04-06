c604b1e34578c96a93c58f2181a88b8b97fca0ec
Simeon Tran <siatran@ucsc.edu>
2024-04-06 15:57:30 -0700

    I am going to make two URIs one for a periodic resource, the another for a non-periodic resource.

--- components/workload/server.c
+++ components/workload/server.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include "workload.h"
 
-void func(void)
-{
+otError periodicResource(otCoapResource *periodic) {
+  periodic->mNext = NULL;
+  periodic->mContext = NULL;
 
+  periodic->mUriPath = "periodic";
+
+  // TO-DO: Create Handler
+  periodic->mHandler = NULL;
+  return OT_ERROR_NONE;
+}
+
+otError aPeriodicResource() {
+  return OT_ERROR_NONE;
 }
