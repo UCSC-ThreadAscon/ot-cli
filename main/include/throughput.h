e06f514f728de45f7436ccc44504cfb8bd3a37fc
Simeon Tran <siatran@ucsc.edu>
2024-06-05 12:39:28 -0700

    Trying to get the ftd to send throughput packets.

--- main/include/throughput.h
+++ main/include/throughput.h
@@ -5,5 +5,5 @@
 #define THROUGHPUT_URI "/throughput"
 #define TP_PAYLOAD_BYTES 4
 
-void tpNonConfirmable(otSockAddr *socket);
+void tpConfirmable(otSockAddr *socket);
 void tpPayload(uint8_t *buffer);
--- main/main.c
+++ main/main.c
@@ -1,9 +1,14 @@
 #include "main.h"
+#include "throughput.c"
 
 void app_main(void)
 {
   startMain();
   coapStart();
 
+  otSockAddr socket;
+  EmptyMemory(&socket, sizeof(otSockAddr));
+
+  tpConfirmable(&socket);
   return;
 }
