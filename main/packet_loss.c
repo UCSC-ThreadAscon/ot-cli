3203008a73d0396920f1bb3cc3c973124f77221d
Simeon Tran <siatran@ucsc.edu>
2024-06-18 16:20:37 -0700

    Print out the sequence number.

--- main/packet_loss.c
+++ main/packet_loss.c
@@ -5,6 +5,8 @@ static otSockAddr socket;
 void plConfirmableSend(otSockAddr *socket)
 {
   static uint32_t sequenceNum = 0;
+  otLogNotePlat("Sending packet with sequence number %" PRIu32 ".\n", sequenceNum);
+
   request(socket, (void *) &sequenceNum, TIGHT_LOOP_PAYLOAD_BYTES,
           PACKET_LOSS_CONFIRMABLE_URI, plConfirmableResponseCallback,
           OT_COAP_TYPE_CONFIRMABLE);
