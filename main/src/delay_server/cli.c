0fa6ffd39d4faeb4e29503a4ab1bfb37ca0055ee
Simeon Tran <siatran@ucsc.edu>
2024-06-23 15:07:02 -0700

    Add .c files will be in src dir

---
+++ main/src/delay_server/cli.c
@@ -0,0 +1,30 @@
+#include "server.h"
+
+static otCoapResource *experimentRoute;
+
+void startCoapServer(uint16_t port) {
+  otError error = otCoapStart(OT_INSTANCE, port);
+
+  if (error != OT_ERROR_NONE) {
+    otLogCritPlat("Failed to start COAP server.");
+  } else {
+    otLogNotePlat("Started CoAP server at port %d.", port);
+  }
+  return;
+}
+
+otError expServerStart(void* aContext, uint8_t argsLength, char* aArgs[]) 
+{
+  checkConnection(OT_INSTANCE);
+  startCoapServer(OT_DEFAULT_COAP_PORT);
+
+  createResource(experimentRoute, "Delay Confirmable", defaultRequestHandler);
+  return OT_ERROR_NONE;
+}
+
+otError expServerFree(void* aContext, uint8_t argsLength, char* aArgs[])
+{
+  resourceDestructor(experimentRoute);
+  otCoapStop(OT_INSTANCE);
+  return OT_ERROR_NONE;
+}
